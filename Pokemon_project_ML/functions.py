9b0ed8af2d70af17175da803458b4a76aed25854
VasileiosKottas <138195958+VasileiosKottas@users.noreply.github.com>
2024-01-15 00:14:50 +0200

    Delete Pokemon_project_ML directory

--- Pokemon_project_ML/functions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def normalize_values(max_value, min_value, data):
-    series = pd.Series()
-    for val in data:
-        d = pd.Series(float((val - min_value) / (max_value - min_value)))
-        series = series._append(d, ignore_index=True)
-    return series
--- Pokemon_project_ML/pdf.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from functions import normalize_values
-
-df = pd.read_csv("pokemon.csv")
-
-type_gen = df['Generation']
-
-pdf_type_gen = normalize_values(type_gen.max(), type_gen.min(), type_gen)
